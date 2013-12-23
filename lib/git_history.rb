2cdeec5e25ac1b72f76d229f55b69dbd20f13a81
Mike West <mwest@mdsol.com>
2013-12-23 14:42:03 -0500

    Refactored GitHub parsing rake task

---
+++ lib/git_history.rb
@@ -0,0 +1,61 @@
+module GitHistory
+  extend self
+
+  # Given a github repo link the repo will be cloned and it's git commit history will
+  # be parsed for further visualization.
+  # If 'path' points to a real GitHub repo then a Repo object will be returned
+  # otherwise nil is returned.
+  def parse(path)
+    begin
+      # tag is the GitHub repo name
+      tag = /\/(.*)\.git/.match(path)[1]
+  #   raise "Repo with tag '#{tag}' is already registered." if Repo.find_by_tag(tag)
+
+      system("cd #{Rails.root}/tmp && git clone #{path} && cd #{tag} && git log > temphist.txt")
+      repo = parse_git_history(tag)
+      repo.commits.reverse.each do |commit|
+        system("cd #{Rails.root}/tmp/#{tag} && git show #{commit.commit_id} > commit#{commit.commit_id}.txt")
+      end
+    ensure
+      puts "Cleaning up..."
+      system("rm -Rf #{Rails.root}/tmp/#{tag}")
+    end
+  end
+
+protected
+  def parse_commit_line(line, keyword)
+    if line.starts_with?(keyword)
+      value = line[(keyword.length)..-1].strip
+      yield(value) if block_given?
+    end
+  end
+
+  # Returns a Repo object
+  def parse_git_history(name)
+    repo = Repo.new(tag: name)
+
+    repo.commits = []
+    current_commit = nil
+    File.readlines("#{Rails.root}/tmp/#{name}/temphist.txt").each do |line|
+      # Start of new commit
+      parse_commit_line(line, 'commit') do |value|
+        current_commit.save && repo.commits << current_commit if current_commit
+
+        current_commit = GitCommit.new(commit_id: value, message: "")
+      end
+
+      parse_commit_line(line, 'Author:') {|value| current_commit.author = value}
+      parse_commit_line(line, 'Date:') {|value| current_commit.commit_date = value}
+
+      #TODO: Store the commit message as well
+      #else
+      #  current_commit.message += line
+      #end
+    end
+
+    current_commit.save && repo.commits << current_commit if current_commit
+
+    repo.save
+    repo
+  end
+end
--- lib/tasks/register.rake
+++ lib/tasks/register.rake
@@ -1,55 +1,9 @@
 namespace :register do
   desc "Register a repo to be visualized"
   task :repo => :environment do
-    begin
-      raise StandardError.new("Must specify a path for the repo") unless ENV['path']
-      path = ENV['path']
+    raise StandardError.new("Must specify a path for the repo") unless ENV['path']
+    path = ENV['path']
 
-      # tag is the GitHub repo name
-      tag = /\/(.*)\.git/.match(path)[1]
-      raise "Repo with tag '#{tag}' is already registered." if Repo.find_by_id(tag)
-
-      system("cd #{Rails.root}/tmp && git clone #{path} && cd #{tag} && git log > temphist.txt")
-      repo = parse_git_history(tag)
-      repo.save
-    ensure
-      puts "Cleaning up..."
-      system("rm -Rf #{Rails.root}/tmp/#{tag}")
-    end
-  end
-
-  # Returns a Repo object
-  def parse_git_history(name)
-    repo = Repo.new(tag: name)
-
-    repo.commits = []
-    current_commit = nil
-    File.readlines("#{Rails.root}/tmp/#{name}/temphist.txt").each do |line|
-      # Start of new commit
-      if line.starts_with?('commit')
-        if current_commit
-          current_commit.save
-          repo.commits << current_commit
-        end
-
-        commit_id = line.split(' ')[1]
-        current_commit = GitCommit.new(commit_id: commit_id, message: "")
-      elsif line.starts_with?('Author:')
-        author = line[7..-1].strip
-        current_commit.author = author
-      elsif line.starts_with?('Date:')
-        date = line[5..-1].strip
-        current_commit.commit_date = date
-      else
-        current_commit.message += line
-      end
-    end
-
-    if current_commit
-      current_commit.save
-      repo.commits << current_commit
-    end
-
-    repo
+    GitHistory.parse(path)
   end
 end
