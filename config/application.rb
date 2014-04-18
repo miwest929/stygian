d3c93a92c4e9ea1c6aa10453d70b2bd40d1597ec
Michael West <mwest@mdsol.com>
2014-04-18 12:25:57 -0400

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'oauth2'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +9,6 @@ Bundler.require(:default, Rails.env)
 
 module Codelife
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/lib/github)
   end
 end
